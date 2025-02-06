1047ee3952aaceb4aec880e4fe65e1a92ac204be
ambic <ambica.mangalagiri@gmail.com>
2025-02-06 11:55:50 +0530

    hhbhj

--- file.tf
+++ file.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "5.81.0"
     }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.5.2"
+    }
   }
 }
 resource "local_file" "pet" {
@@ -21,3 +25,23 @@ resource "aws_s3_bucket" "data" {
   bucket = "feb-06-2025"
   acl = "private"
 }
+
+resource "local_file" "games" {
+file     = "/root/favorite-games"
+content  = "FIFA 21"
+}
+
+resource "local_file" "pets" {
+  filename = "/root/pets.txt"
+  content = "this for creation of pets file"
+  file_permission = "0777"
+}
+
+resource "local_sensitive_file" "games" {
+  filename     = "/root/favorite-games"
+  content  = "FIFA 21"
+  # sensitive_content = "FIFA 21"
+}
+
+
+
